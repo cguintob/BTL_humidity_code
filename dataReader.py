56dccee0c8a9bc92776b54001b2b81f78a2e345f
Bryan Cardwell <bryan.cardwell@cern.ch>
2024-02-29 15:13:23 -0500

    Littering with comments

--- dataReader.py
+++ dataReader.py
@@ -1,13 +1,25 @@
+''' This script takes the data from the Arduino that was put
+into a data file by sensorData.py and plots it. When running
+this script, we do "python dataReader.py," and it will plot
+humidity and temperature graphs over time to the screen. '''
+
+# These packages are useful for plotting and for math.
 import matplotlib.pyplot as plt
 import numpy as np
 
-file = "data_file5.txt"
+# Like in sensorData.py, this variable prevents us from changing too much code.
+file = "data_one-sec_1.txt"
 
+# These lists will adopt the appropriate values from our data file.
 index = []
 times = []
 humidities = []
 temps = []
 
+''' This for loop puts the appropriate data into the appropriate lists.
+Note that times[] contains both the data from the second column and the
+data from the third column, separated by a newline character. This will
+be useful for plotting the labels along the x-axis. '''
 for line in open(file, "r"):
     lines = [i for i in line.split()]
     index.append(lines[0])
@@ -15,6 +27,7 @@ for line in open(file, "r"):
     humidities.append(lines[3])
     temps.append(lines[4])
 
+# This section of code plots the humidities.
 hum = plt.figure(1)
 plt.title("Humidities at Various Times")
 plt.ylabel("Relative Humidity (%)")
@@ -22,6 +35,7 @@ plt.xticks(np.arange(len(index), step = len(index) / 10), times, fontsize = 8, r
 plt.plot(index, humidities, marker = "o", c = "g")
 hum.show()
 
+# This section of code plots the temperatures.
 temp = plt.figure(2)
 plt.title("Temperatures at Various Times")
 plt.ylabel("Temperature (C)")
@@ -29,10 +43,19 @@ plt.xticks(np.arange(len(index), step = len(index) / 10), times, fontsize = 8, r
 plt.plot(index, temps, marker = "o", c = "g")
 temp.show()
 
+# This command is necessary for showing the plots separately, for some reason.
 input()
 
 
 
+
+
+
+'''-----------------------------------------------------------'''
+
+
+
+''' Like in sensorData.py, the code from here on are ideas I was testing. '''
 
 # x = [dt.datetime.strptime(d, '%Y-%m-%d').date() for d in dates]
 
--- sensorData.py
+++ sensorData.py
@@ -1,33 +1,76 @@
+''' This script talks with the Arduino to create a data file
+that contains information about the relative humidity and
+temperature in the production room at points in time. When
+the Arduino sends its data to the "server," this script
+(which is run with "python sensorData.py" after the Arduino
+code has finished uploading) takes the data, splices it,
+and puts each value into a data file when the value arrives. '''
+
+
+''' "Serial" lets the Arduino send its information to the server,
+where the Python script can access it. "Datetime" contains
+information about the date and time of either the current
+moment or moments in the past and future. "Date" is a module
+in "Datetime" that specifically accesses the date. '''
 import serial
 import datetime
 from datetime import date
 
+''' This function reads the data stream from the Arduino. The 
+COM port tells the program where to look for the data, the 
+baudrate lets the program synchronize with the Arduino, while
+the timestamp tells you when the data was collected. In the 
+serial.Serial function, "Serial" must be a module in "serial"
+that directs the program to the information from the Arduino.
+Also,  1/timeout is the frequency at which the port is read. '''
 def readserial(comport, baudrate, timestamp = False):
     ser = serial.Serial(comport, baudrate, timeout = 0.1)  
-    # 1/timeout is the frequency at which the port is read
 
+    # This counter is used to determine what parameter the values represent.
     counter = 0
 
-    file = "data_file7.txt"
+    # This variable lets us change the data file without modifying too much code.
+    file = "data_one-sec_1.txt"
 
+    ''' This section of code empties the data file each time it is 
+    run so that only the data from the most recent run are collected. '''
     init_file = open(file, "w")
     init_file.write("")
     init_file.close
 
+    # This while loop reads the code if the Arduino sends data to it.
     while True:
+
+        ''' Each data point is taken from serial, read, decoded, 
+        and stripped into a format the Python program can understand. ''' 
         data = ser.readline().decode().strip()
+        
+        ''' The Arduino sends some extraneous information I don't want 
+        in the data file, but I let the Python program report it. '''
         if ((data == "Starting up...") or (data == "Sensor not running.") or (data == "AHT10 running")):
             print(data)
+
+        # These things are sent by the Arduino and shouldn't be included.
         elif ((data == "") or (data == "0.00") or (data == "-50.00") or (data == "..") or (data == "up..")):
             continue
+
+        # This is only run if we're collecting a set number of measurements.
         elif (data == "Done!"):
             print(data)
             break
+
+        # This actually puts the data into a data file.
         else:
+
+            # These are messages that confirm that the program is working.
             if (counter % 10 == 0):
                 print("Gathering data. Please be patient. It's working, I promise.")
+            
+            # Day gives the current date; time gives the current time in hours:minutes.
             day = date.today()
             time = datetime.datetime.now().strftime("%H:%M")
+            
+            # This writes everything except the temperature to the data file.
             if (counter % 2 == 0):
                 data_file = open(file, "a")
                 data_file.write(str(int(0.5 * counter)))
@@ -38,16 +81,36 @@ def readserial(comport, baudrate, timestamp = False):
                 data_file.write(" ")
                 data_file.write(str(data))
                 data_file.write(" ")
+
+            # This writes the temperature to the data file.
             else:
                 data_file.write(str(data))
                 data_file.write("\n")
                 data_file.close()
             counter += 1
 
+''' It's important to note that I open and close the data file each
+time I add a new set of measurements (hence why I use "a" instead of
+"w". I did this in case we need to stop data collection but didn't
+want to lose our data. By continuously opening and closing the file,
+we save the data, preventing it from being lost. '''
+
+# This tells the program where to look for the AHT10 program.
 if __name__ == '__main__':
     readserial('/dev/ttyACM0', 9600, True)
     # COM port, Baudrate, Show timestamp
     
+
+
+
+
+
+
+
+'''-------------------------------------------------------------------------'''
+
+
+
 
 
 
